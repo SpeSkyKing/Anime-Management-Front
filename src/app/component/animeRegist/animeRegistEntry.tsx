17cfd40eca9889e9349bc90261fcab7d4ed18013
SPELLSKY <hongmingjitian272@gmail.com>
2025-01-18 07:32:01 +0900

    ⚡️ アニメ登録処理　細かい部分の修正

--- src/app/component/animeRegist/animeRegistEntry.tsx
+++ src/app/component/animeRegist/animeRegistEntry.tsx
@@ -29,7 +29,7 @@ const AnimeRegistEntry = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-           Authorization: `Bearer ${token}`,
+          "Authorization": `Bearer ${token}`,
         },
         body: JSON.stringify(formData),
       });
